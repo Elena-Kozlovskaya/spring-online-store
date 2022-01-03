494629e8d37a6974c1599ac44eea121235d939c0
Elena Kozlovskaya <lenadk23@gmail.com>
2022-01-03 21:36:28 +0500

    Added classwork

--- src/main/resources/db/migration/V3__insert security.sql
+++ src/main/resources/db/migration/V3__insert security.sql
@@ -56,3 +56,21 @@ insert into roles_permissions (role_id, permission_id)
 values (1, 2),
        (2, 1),
        (2, 2);
+
+create table if not exists orders (
+    id              bigserial primary key,
+    user_id         bigint not null references users (id),
+    total_price     int not null,
+    address         varchar(255),
+    phone           varchar(255)
+)
+
+create table if not exists order_items (
+    id                      bigserial primary key,
+    product_id              bigint not null references products (id),
+    user_id                 bigint not null references users (id),
+    order_id                bigint not null references orders (id),
+    quantity                int not null,
+    price_per_product       int not null,
+    price                   int not null
+)
