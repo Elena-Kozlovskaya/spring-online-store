6261e25cf3d300263f151c2a89e60ae5f2a6e84f
Elena Kozlovskaya <lenadk23@gmail.com>
2022-01-12 20:44:12 +0500

    Lesson_1

--- src/main/java/com/kozlovskaya/spring/online/store/entities/Product.java
+++ src/main/java/com/kozlovskaya/spring/online/store/entities/Product.java
@@ -1,6 +1,5 @@
 package com.kozlovskaya.spring.online.store.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -8,13 +7,11 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Table(name = "products")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,15 +24,6 @@ public class Product {
     @Column(name = "cost")
     private Integer cost;
 
-    @OneToMany(mappedBy = "product")
-    List<OrderItem> orderItems;
-
-    public Product(Long id, String title, Integer cost) {
-        this.id = id;
-        this.title = title;
-        this.cost = cost;
-    }
-
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
@@ -43,4 +31,10 @@ public class Product {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public Product(Long id, String title, Integer cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
 }
